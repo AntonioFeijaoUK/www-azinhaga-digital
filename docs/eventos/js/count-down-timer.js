07b4af0017a12938fb0c1ce9818802a58aa893ab
Antonio Feijao <2548084+AntonioFeijaoUK@users.noreply.github.com>
2022-09-29 20:59:45 -0400

    Update count-down-timer.js

--- docs/eventos/js/count-down-timer.js
+++ docs/eventos/js/count-down-timer.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("September 10, 2022 10:00:00").getTime();
+var countDownDate = new Date("April 8, 2023 10:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -23,6 +23,6 @@ var x = setInterval(function() {
   // If the count down is over, write some text 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("TIMER").innerHTML = "LET THE GAME BEGIN!";
+    document.getElementById("TIMER").innerHTML = "";
   }
 }, 1000);
